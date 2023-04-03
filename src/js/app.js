852bd55ff6187c7717b39a484fb020dc71c69476
AlenEvtikhov <50318546+AlenEvtikhov@users.noreply.github.com>
2023-04-03 12:59:22 +0300

    add files

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import addCard from './addCard.js';
+import managerCard from './managerCard.js';
+import dndCard from './dndCard.js';
+import {
+  loadStorage,
+} from './storage.js';
+
+// console.log('app.js bundled');
+
+dndCard();
+loadStorage();
+managerCard();
+addCard();
+window.addEventListener('load', loadStorage());
